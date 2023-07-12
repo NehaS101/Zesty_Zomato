567d315d36df9814eeb44a56e0727a3669be8ccc
NehaS101 <solankineha297@gmail.com>
2023-07-12 14:18:52 +0530

    updated main loop

---
+++ index.py
@@ -0,0 +1,14 @@
+
+
+#main loop
+while True:
+    print(f"\nwelcome to Zesty Zomato")
+    print("what you would like to do")
+    print("1. Add a dish to your menu")
+    print("2. Remove a dish from your menu")
+    print("3. Update the availability of dish")
+    print("4. Take new order")
+    print("5. Update the status of an order")
+    print("6. Review all orders")
+    
+    choice = input("Enter you choice: ")
